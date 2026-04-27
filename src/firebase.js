515dd80a4749b2bb7c5861ce36dc03fa21c389b3
Kartik <devilkartik2580@gmail.com>
2026-04-28 01:02:02 +0530

    Fix Firestore initialization crash on deployed app

--- src/firebase.js
+++ src/firebase.js
@@ -18,7 +18,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = initializeFirestore(app, {
   localCache: memoryLocalCache(),
-  experimentalAutoDetectLongPolling: true,
   experimentalForceLongPolling: true,
-  useFetchStreams: false,
 });
